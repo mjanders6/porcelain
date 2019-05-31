e4c76ddb9dc44f5b21e3a2f2bff04a1086a7bdea
unknown <summerbasehart@gmail.com>
2019-05-30 18:15:03 -0700

    updated AddBR

--- src/components/AddBR/AddBR.js
+++ src/components/AddBR/AddBR.js
@@ -1,12 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import { Nav } from 'react-bootstrap'
 import AddBathroomForm from '../AddBathroomForm/AddBathroomForm.jsx'
 
 // Need to create a add icon
 function AddBR() {
     return (
-        // <Nav.Link href="../AddBathroomForm/AddBathroomForm.jsx">AddBR</Nav.Link>
-        <AddBathroomForm></AddBathroomForm>
+        <li> <Link to="../AddBathroomForm/AddBathroomForm.jsx">AddBR</Link> </li>
     )
 }
 
