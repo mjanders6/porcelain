7c150731addac51b9289ccc062f6c38687d28ede
Sarika <sarika@ngoyal.com>
2019-05-31 21:27:31 -0700

    Sub Menu

---
+++ client/src/pages/Profile/Profile-sub-level/ProfileComments/ProfileComments.js
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+
+
+
+class ProfileComments extends Component {
+    render() {
+      return <h1>Hello, Profile Comments</h1>;
+    }
+  }
+
+  export default ProfileComments
---
+++ client/src/pages/Profile/Profile-sub-level/ProfileLikes/ProfileLikes.js
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+
+
+
+class ProfileLikes extends Component {
+    render() {
+      return <h1>Hello, ProfileLikes</h1>;
+    }
+  }
+
+  export default ProfileLikes
